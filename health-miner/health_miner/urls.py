717df2c240e29ba2047b021cb7d08caa254e39c6
gdh1995 <gdh1995@qq.com>
2015-10-26 23:54:40 +0800

    add weixin URL access

--- health-miner/health_miner/urls.py
+++ health-miner/health_miner/urls.py
@@ -16,6 +16,29 @@ HEALTNIC = settings.HEALTNIC_RREFIX
 def home(request):
     return HttpResponsePermanentRedirect("/" + MHELP + "/")
 
+import hashlib
+
+def validate(request):
+    signature =  request.GET.get("signature")
+    timestamp = request.GET.get("timestamp")
+    nonce = request.GET.get("nonce")
+    token = 'healthnic'
+    tmpArr = [token, timestamp, nonce]
+    tmpArr.sort()
+    tmpStr = ''.join(tmpArr)
+    tmpStr = hashlib.sha1(tmpStr).hexdigest()
+    return tmpStr == signature
+
+def on_weixin(request):
+    if request.method == 'POST':
+        return HttpResponse("NO POST")
+    elif request.GET.get("signature"): #access token
+        if validate(request):
+            return HttpResponse(request.GET.get('echostr'))
+        else:
+            return HttpResponse('false')
+    return HttpResponse("Unknown Method")
+
 def redirect_tp_wx(request):
     return HttpResponsePermanentRedirect("http://www.healthnic.cn:8000" + request.get_full_path())
 
@@ -27,7 +50,7 @@ urlpatterns = patterns('',
     url(r'^' + MHELP +  r'$',  home),
     url(r'^' + MHELP + r'/',   include('health_miner.mhelp_urls')),
     url(r'^wx/',  redirect_tp_wx),
-    url(r'^weixin',  redirect_tp_wx),
+    url(r'^weixin',  on_weixin),
 )
 
 if settings.DEBUG:
